94ca27a93640582d35ba422914e7bbcf5aa1ffe6
samuel1Y <samuel.bajzik0@gmail.com>
2021-02-25 11:04:35 +0100

    sheet class update 2

--- src/Sheet.java
+++ src/Sheet.java
@@ -15,7 +15,7 @@ public class Sheet
 
   public void setScore(int rowIndex, int[] diceNumbers)
   {
-
+    score[rowIndex] = calculateScore(rowIndex, diceNumbers);
   }
   public int getTotalScore()
   {
@@ -43,29 +43,40 @@ public class Sheet
     switch(rowIndex)
     {
       case 0:
+        return diceSum(1, diceNum);
       case 1:
+        return diceSum(2, diceNum);
       case 2:
+        return diceSum(3, diceNum);
       case 3:
+        return diceSum(4, diceNum);
       case 4:
+        return diceSum(5, diceNum);
       case 5:
+        return diceSum(6, diceNum);
       case 6:
+        return 35;
       case 7:
       case 8:
-      case 9:
-      case 10:
-      case 11:
-      case 12:
       case 13:
-      case 14:
-      case 15:
-      case 16:
+        return diceSumAll(diceNum);
 
+      case 9:
+        return 25;
+      case 10:
+        return 30;
+      case 11:
+        return 40;
+      case 12:
+        return 50;
+      case 14:
+        return 100;
     }
     return 0;
   }
   public void addYahtzeeBonus()
   {
-
+    score[14] += 100;
   }
 
   public int diceSum(int diceNumber, int[] diceNum)
@@ -76,6 +87,47 @@ public class Sheet
       if (diceNum[i] == diceNumber)
       {
         sum+= diceNum[i];
+      }
+    }
+    return sum;
+  }
+
+  public int diceSumAll(int[] diceNum)
+  {
+    int sum = 0;
+    for( int i = 0; i < diceNum.length; i++ )
+    {
+        sum+= diceNum[i];
+    }
+    return sum;
+  }
+
+  public int countDice(int diceNumber, int[] diceNum)
+  {
+    int sum = 0;
+
+    for( int i = 0; i < diceNum.length; i++ )
+    {
+      switch(diceNumber)
+      {
+        case 1:
+          if(diceNum[i] == 1) sum++;
+          break;
+        case 2:
+          if(diceNum[i] == 2) sum++;
+          break;
+        case 3:
+          if(diceNum[i] == 3) sum++;
+          break;
+        case 4:
+          if(diceNum[i] == 4) sum++;
+          break;
+        case 5:
+          if(diceNum[i] == 5) sum++;
+          break;
+        case 6:
+          if(diceNum[i] == 6) sum++;
+          break;
       }
     }
     return sum;
