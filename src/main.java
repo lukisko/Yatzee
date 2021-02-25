afb0238e00a4c8db5349ed91fc713ea239a6e6ed
lukisko <lukas.pleva@mail.com>
2021-02-25 09:55:32 +0100

    player roll dices

--- src/main.java
+++ src/main.java
@@ -1,21 +1,23 @@
+import java.util.Scanner;
+
 public class main
 {
   public static void main(String[] args)
   {
-    Dice dic1 = new Dice();
-    dic1.roll();
-    System.out.println(dic1.getValue());
-    dic1.roll();
-    System.out.println(dic1.getValue());
-    dic1.roll();
-    System.out.println(dic1.getValue());
-    dic1.roll();
-    System.out.println(dic1.getValue());
-    dic1.roll();
-    System.out.println(dic1.getValue());
-    dic1.roll();
-    System.out.println(dic1.getValue());dic1.roll();
-    System.out.println(dic1.getValue());
-
+    Player p1 = new Player();
+    Scanner in = new Scanner(System.in);
+    System.out.println("Hi, let's play a game!");
+    while (true){
+      System.out.println("type r to roll");
+      String input = in.nextLine();
+      if (input.equals("r")){
+        p1.roll();
+        Dice[] dices = p1.getDices();
+        for (int i = 0; i < 6; i++)
+        {
+          System.out.println("cube #"+i+ " : "+ dices[i].getValue());
+        }
+      }
+    }
   }
 }
