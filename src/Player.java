21b261e712975b042b210c0a3164cdc635d5cea9
lukisko <lukas.pleva@mail.com>
2021-02-25 09:35:50 +0100

    player has cubes

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,14 @@
 public class Player
 {
   private int rollsRemain;
+  private Dice[] dices;
 
   public Player(){
+    dices = new Dice[5];
+    for (int i = 0; i < 6; i++)
+    {
+      dices[i] = new Dice();
+    }
     rollsRemain= 3;
   }
 }
